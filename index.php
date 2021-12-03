b6307636f7fca3aeaea866c8a8f77d310c8391d9
4NUB!S <xexjz0leechersteamofficialclub@gmail.com>
2021-12-03 14:58:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -48,10 +48,16 @@
          
             var url = "https://cj-android-demon.herokuapp.com/json2.php";
             console.log(url);
-            $.get(url, (data, status)=>{
-                console.log(data);
-                
-            })
+            $.getJSON(url)
+                .done((data)=>{
+                    $.each(data, (k, item)=>{
+                        console.log(item);
+                        console.log(item.fname);
+                    });
+                })
+                .fail((shr, status, err)=>{
+                    console.log(err);
+                });
         });
     });
 </script>
