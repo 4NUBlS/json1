a9cd53a1fdeb68e6ffa0e53f7fb7e0d681c16b4b
4NUB!S <xexjz0leechersteamofficialclub@gmail.com>
2021-12-07 11:27:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,25 +8,34 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <button id="btnJSON1"> JSON 1 </button>
-    Name : <span id="fname"></span> <br/>
-    Sname: <span id="lname"></span>
+ 
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th><th>First Name</th><th>Last Name</th>
+            </tr>
+        </thead>
+        <tbody id="tblStudent">
+        </tbody>
+    </table>
 </body>
 <script>
+ 
 function loadJSON(){
-    var url = "https://cj-android-demon.herokuapp.com/json2.php";
+    var url = "https://cj-android-demon.herokuapp.com/json1.php";
+
     $.getJSON(url)
         .done((data)=>{
             console.log(data);
-            console.log(data[1]);
-            console.log(data[2].fname);
-          //  $("#fname").text(data.fname);
         })
         .fail((xhr, status, err)=>{
+
         });
 }
+
 $(()=>{
-    $("#btnJSON1").click(loadJSON);
+    loadJSON();
 });
 </script>
+
 </html>
